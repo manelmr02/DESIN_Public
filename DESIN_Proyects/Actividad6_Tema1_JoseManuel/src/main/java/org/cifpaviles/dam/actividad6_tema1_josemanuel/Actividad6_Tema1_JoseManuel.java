0cfce9f7d759b6cab029e82e6fa2489102ec92fa
Manel Morales Rivera <manelmoralesrivera@gmail.com>
2025-11-17 22:25:11 +0100

    Subida de la Actividad 6
    Subida de la Actividad 6 de la Unidad 1 de DESIN

---
+++ DESIN_Proyects/Actividad6_Tema1_JoseManuel/src/main/java/org/cifpaviles/dam/actividad6_tema1_josemanuel/Actividad6_Tema1_JoseManuel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.cifpaviles.dam.actividad6_tema1_josemanuel;
+
+/**
+ *
+ * @author josemmr
+ */
+public class Actividad6_Tema1_JoseManuel {
+
+    public static void main(String[] args) {
+       javax.swing.SwingUtilities.invokeLater(() -> {
+            new VentanaPrincipal().setVisible(true);
+        });
+    }
+}
